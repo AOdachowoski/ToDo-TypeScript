2bdb46fa1910367d635e7b1a9268b3583add96e7
Amanda Karoline Odachowoski <100538853+AOdachowoski@users.noreply.github.com>
2023-02-27 11:04:22 -0300

    Add files via upload

---
+++ src/App.styles.ts
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  background-color: #0f0f0f;
+  color: #fefefe;
+  min-height: 100vh;
+`;
+
+export const Content = styled.div`
+  margin: auto;
+  max-width: 980px;
+  padding: 20px 10px;
+`;
+
+export const Header = styled.h1`
+  margin: 0;
+  padding: 0;
+  text-align: center;
+  color: #fefefe;
+  border-bottom: 1px solid #444;
+  padding-bottom: 20px;
+`;
